452e280a029542d51f695b7dbe6765514976cc6c
sean snel <seansnel@hotmail.com>
2025-08-20 21:43:18 +0200

    glitter

--- js/gallery.js
+++ js/gallery.js
@@ -1,31 +1,28 @@
-const photos = ["assets/fluffy_pomeriaan_in_cartoon_stijl.png"];
+const photos = ["fluffy_pomeriaan_in_cartoon_stijl.png"];
 const gallery = document.getElementById("gallery");
 
 const likedPhotos = JSON.parse(localStorage.getItem("likedPhotos") || "{}");
 
-photos.forEach(photo => {
+photos.forEach((photo) => {
   const container = document.createElement("div");
-  container.classList.add("photo-container");
+  container.classList.add("container");
 
   const img = document.createElement("img");
   img.src = `assets/${photo}`;
   img.alt = photo;
 
-  
   const likeBtn = document.createElement("span");
   likeBtn.classList.add("like-btn");
   likeBtn.innerHTML = "♥";
 
-  
   if (likedPhotos[photo]) {
     likeBtn.style.color = "pink";
   } else {
     likeBtn.style.color = "grey";
   }
 
-  
   likeBtn.addEventListener("click", () => {
-    likedPhotos[photo] = !likedPhotos[photo]; 
+    likedPhotos[photo] = !likedPhotos[photo];
     likeBtn.style.color = likedPhotos[photo] ? "pink" : "grey";
     localStorage.setItem("likedPhotos", JSON.stringify(likedPhotos));
   });
